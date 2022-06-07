e0ddcc991d3c761227f40939b9d48b1afcc4601f
sujeet kumar <rajrock7254@gnail.com>
2022-06-07 08:09:16 -0400

    have issue on heroku that TypeError: Cannot read properties of undefined (reading '0') while displaying array of an object than sshowing that error

--- src/index.js
+++ src/index.js
@@ -67,10 +67,10 @@ app.post('/search',(req,res)=>{
                 name:rs.data[0].name,
                 city:rs.data[0].city,
                 imgSrc:rs.data[0].image,
-                id:rs.data[0].id,
-                 numberType:rs.data[0].phones[0].numberType,
+                id:rs.data[0].id
+            /*      numberType:rs.data[0].phones[0].numberType,
                  timeZone:rs.data[0].addresses[0].timeZone,
-                carrier:rs.data[0].phones[0].carrier
+                carrier:rs.data[0].phones[0].carrier */
             })
         });
       
