4f52f9dd4d4b8dbbe33b00816ffe59b37f56dd7f
sujeet kumar <rajrock7254@gnail.com>
2022-06-07 07:50:43 -0400

    Address problen

--- src/index.js
+++ src/index.js
@@ -65,7 +65,7 @@ app.post('/search',(req,res)=>{
                 title:" Search About Phone Number IND",
                 access:rs.data[0].access, 
                 name:rs.data[0].name,
-                city:rs.data[0].addresses[0].city,
+                city:rs.data[0].city,
                 imgSrc:rs.data[0].image,
                 id:rs.data[0].id,
                  numberType:rs.data[0].phones[0].numberType,
