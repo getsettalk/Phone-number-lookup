5942c5837fc07bd72a83fb4cd6c61e6b13273e1c
sujeet kumar <rajrock7254@gnail.com>
2022-06-24 19:57:32 -0400

    added more info

--- src/index.js
+++ src/index.js
@@ -58,8 +58,9 @@ app.post('/search',(req,res)=>{
         sn.then(function(response) {
         
             var rs= JSON.parse(response);
-             console.log(rs);
-            // res.send(rs.data[0].name)
+             console.log(rs.data[0].addresses[0].timeZone);
+             console.log(rs.data[0].phones[0].carrier );
+            //res.send(rs.data[0].phones[0].numberType)
             res.render("search",{
                 phone:ph,
                 title:" Search About Phone Number IND",
@@ -67,10 +68,10 @@ app.post('/search',(req,res)=>{
                 name:rs.data[0].name,
                 city:rs.data[0].city,
                 imgSrc:rs.data[0].image,
-                id:rs.data[0].id
-            /*      numberType:rs.data[0].phones[0].numberType,
+                id:rs.data[0].id,
+                 numberType:rs.data[0].phones[0].numberType,
                  timeZone:rs.data[0].addresses[0].timeZone,
-                carrier:rs.data[0].phones[0].carrier */
+                carrier:rs.data[0].phones[0].carrier 
             })
         });
       
